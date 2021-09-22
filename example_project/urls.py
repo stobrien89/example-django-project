28d963eadffb3327cac66b31cd07ec176070e099
stobrien89 <stobrien@crimson.ua.edu>
2021-09-22 16:15:41 -0700

    add first view/route

--- example_project/urls.py
+++ example_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app.views import FirstView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/', FirstView.as_view())
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -5,4 +5,11 @@ from django.views import View
 # Create your views here.
 
 class FirstView(View):
-    pass
+    def get(self, request):
+        return JsonResponse({"hello", "world", "method": request.method})
+    def post(self, request):
+        return JsonResponse({"hello", "world", "method": request.method})
+    def put(self, request):
+        return JsonResponse({"hello", "world", "method": request.method})
+    def delete(self, request):
+        return JsonResponse({"hello", "world", "method": request.method})
